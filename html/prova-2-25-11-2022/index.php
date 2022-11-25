a551f55c6e78e8d2b35a7774fd70b906810b1117
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2022-11-25 08:34:18 -0300

    prova-2-25-11-2022

---
+++ html/prova-2-25-11-2022/index.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Respostas:
+ * Questão 1: C
+ * Questão 2: B
+ * Questão 3: C
+ * Questão 4: Falso
+ * Questão 5: Verdadeiro
+ * Questão 6: Falso
+ * Questão 7: Verdadeiro
+ * Questão 8: Emmanuel...
+ * Questão 9: ...
+ */
+
+/**
+ * Questão 2
+ */
+// $produto = array( 1, 2);
+// $produto[0] = "SSD"; $produto[1] = "HD"; $produto[2] = "mouse";
+// for ($i == 0; $i<3;$i++)
+//     {echo "$i = $produto[$i]<br />";}
+
+ /**
+  * Questão 1
+  */
+// echo <<<TEXTO
+// <pre>
+// <br>
+// Indique as capacidades que deseja:
+//     <br>
+//     Tela <input type="text" name="tela">
+//     <br>
+//     SSD <input type="text" name="ssd">
+//     <br>
+//     HD  <input type="text" name="area">
+//     <form name = "laptop" action="fazAlgo" method="post">
+//     <input type="submit" value="Por no carrinho">
+// </pre></form>
+// _TEXTO;
+// ?>
