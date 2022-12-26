ffb4ec5ee9a3a392a4a1766dba722eb7798f8cbc
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2022-12-26 09:46:07 -0300

    prova-3-13-12-2022

---
+++ html/prova-3-13-12-2022/index.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+    Q1
+        B-) F, V, F, F
+
+
+    Q2
+        D-) F, V, F, F
+
+
+    Q3
+        B-) usuário
+
+
+    Q4
+        M-) senha
+        
+
+    Q5
+        F-) BD
+
+
+    Q6
+        C-) query
+
+
+    Q7
+        O-) Nenhuma das anteriores
+
+
+    Q8
+        E-) fetch_assoc
+
+
+    Q9
+        RA
+
+    Q9
+        EMMANUEL VINÍCIUS BERGMANN DE FIGUEIREDO
+ */
+
+// var_dump(10 == "10");
+// var_dump(10 === "10");
